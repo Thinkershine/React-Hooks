e3932a7f48e34e64426539a6147957fdbd9ea64d
Sebastian Siemaszko <siemaszkosebastian@gmail.com>
2019-08-08 07:43:31 +0200

    Add ClickCounter with React.useState

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ClickCounter from "./../src/clickCounter";
 
 const App = () => {
   return (
     <div>
       <h1>Hello to Hooks with React!</h1>
+      <ClickCounter />
     </div>
   );
 };
---
+++ src/clickCounter.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ClickCounter = () => {
+  const [count, setCount] = React.useState(0);
+  const [clickCount, setClickCount] = React.useState(0);
+
+  const increaseCounter = () => {
+    setCount(count + 1);
+    setClickCount(clickCount + 1);
+  };
+
+  const decreaseCounter = () => {
+    setCount(count - 1);
+    setClickCount(clickCount + 1);
+  };
+
+  const style = {
+    border: "1px  solid black",
+    padding: 10,
+    margin: 5
+  };
+
+  return (
+    <div style={style}>
+      <h1>Click Counter</h1>
+      <hr />
+      <h2>Count : {count}</h2>
+      <h2>Total Clicks : {clickCount}</h2>
+      <hr />
+      <button onClick={increaseCounter}>Increase</button>
+      <button onClick={decreaseCounter}>Increase</button>
+    </div>
+  );
+};
+
+export default ClickCounter;
